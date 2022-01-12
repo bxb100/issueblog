b9caf5972e531d20fead3229e732ed1a511d9dca
John <bxb100@outlook.com>
2022-01-12 20:46:19 +0800

    fix: backup file style

--- dist/index.js
+++ dist/index.js
@@ -539,7 +539,7 @@ function saveIssue(kit, issue, info) {
                 .then(list => list.filter(c => (0, util_1.isOwnBy)(c, kit.owner)));
             for (const comment of comments) {
                 content += `\n\n---\n\n`;
-                content += `<a href="#issuecomment-${comment.id}"></a>`;
+                content += `<a id="issuecomment-${comment.id}"></a>\n`;
                 content += comment.body;
             }
         }
