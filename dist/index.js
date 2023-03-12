94c05ec475a72ef5eef352f48b846372e475c163
Lemon <bxb100@outlook.com>
2023-03-12 17:05:32 +0800

    fix: hexo title contain colon

--- dist/index.js
+++ dist/index.js
@@ -624,7 +624,9 @@ function saveIssue(kit, issue, info, BACKUP_PATH) {
             .join('');
         // hexo simple post template
         const createAt = new Date(issue.created_at).getTime();
-        let content = `---\ntitle: ${issue.title}\ndate: ${createAt}\ntags:\n${tags}\nurl: ${issue.html_url}\n\n---\n`;
+        // hexo not support : in title
+        const mdTitle = issue.title.replace(/:/g, ' ');
+        let content = `---\ntitle: ${mdTitle}\ndate: ${createAt}\ntags:\n${tags}\nurl: ${issue.html_url}\n\n---\n`;
         content += issue.body || '';
         if (issue.comments > 0) {
             // just focus on the first hundred comments
--- src/functions/files.ts
+++ src/functions/files.ts
@@ -77,7 +77,9 @@ async function saveIssue(
 
     // hexo simple post template
     const createAt = new Date(issue.created_at).getTime()
-    let content = `---\ntitle: ${issue.title}\ndate: ${createAt}\ntags:\n${tags}\nurl: ${issue.html_url}\n\n---\n`
+    // hexo not support : in title
+    const mdTitle = issue.title.replace(/:/g, ' ')
+    let content = `---\ntitle: ${mdTitle}\ndate: ${createAt}\ntags:\n${tags}\nurl: ${issue.html_url}\n\n---\n`
     content += issue.body || ''
     if (issue.comments > 0) {
         // just focus on the first hundred comments
